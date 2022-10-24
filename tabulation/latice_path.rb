a3d77a02fe8fdedc43b6ca5ad2a188ab500b1ddc
Henok Teklu <henokzion@gmail.com>
2022-10-24 21:50:10 +0300

    refactor latice path tabulation

--- tabulation/latice_path.rb
+++ tabulation/latice_path.rb
@@ -4,14 +4,8 @@ def count_path(n)
 
   latice.each_with_index do |row, x|
     row.each_with_index do |column, y|
-      if x < n && y < n
-        latice[x+1][y] += latice[x][y]
-        latice[x][y+1] += latice[x][y]
-      elsif x < n
-        latice[x+1][y] += latice[x][y]
-      elsif y < n
-        latice[x][y+1] += latice[x][y]
-      end
+      latice[x+1][y] += latice[x][y] unless latice[x+1][y].nil?
+      latice[x][y+1] += latice[x][y] unless latice[x][y+1].nil?
     end
   end
   latice.last.last
